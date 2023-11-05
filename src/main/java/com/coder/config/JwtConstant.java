e6c4c0a9b2b494c80970c3843afe7965db2ab2de
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-05 17:04:52 +0530

    JWT Token config

---
+++ src/main/java/com/coder/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.coder.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY = "rabisahoonarayansahoohtianzwrgbhjq";
+	public static final String JWT_HEADER = "Authorization";
+}
