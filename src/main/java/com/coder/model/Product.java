92d7c37f1fdefd9786664d387ac49d6e6087a283
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-06 18:02:08 +0530

    product model

--- src/main/java/com/coder/model/Product.java
+++ src/main/java/com/coder/model/Product.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale.Category;
+
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
