e25a194a6f8b18bc12020f92b99975cda93d3083
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-09 18:52:51 +0530

    CartServiceImplementation

--- src/main/java/com/coder/repository/CartRepository.java
+++ src/main/java/com/coder/repository/CartRepository.java
@@ -1,9 +1,13 @@
 package com.coder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.coder.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Long>{
 
+	@Query("SELECT c From Cart c Where c.user.id=:userId")
+	public Cart findByUserId(@Param("userId")Long userId);
 }
--- src/main/java/com/coder/service/CartServiceImplementation.java
+++ src/main/java/com/coder/service/CartServiceImplementation.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.coder.exception.ProductException;
 import com.coder.model.Cart;
+import com.coder.model.CartItem;
+import com.coder.model.Product;
 import com.coder.model.User;
 import com.coder.repository.CartRepository;
 import com.coder.request.AddItemRequest;
@@ -15,22 +17,72 @@ public class CartServiceImplementation implements CartService{
 	
 	private CartItemService cartItemService;
 	
+	private ProductService productService;
+	
+	public CartServiceImplementation(CartRepository cartRepository, CartItemService cartItemService, ProductService productService) {
+		this.cartRepository = cartRepository;
+		this.cartItemService = cartItemService;
+		this.productService = productService;
+	}
+	
 	@Override
 	public Cart createCart(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Cart cart = new Cart();
+		cart.setUser(user);
+		
+		return cartRepository.save(cart);
 	}
 
 	@Override
 	public String addCartItem(Long userId, AddItemRequest req) throws ProductException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Cart cart = cartRepository.findByUserId(userId);
+		
+		Product product = productService.findProductById(req.getProductId());
+		
+		CartItem isPresent = cartItemService.isCartItemExist(cart, product, req.getSize(), userId);
+		
+		if(isPresent == null) {
+			CartItem cartItem = new CartItem();
+			cartItem.setProduct(product);
+			cartItem.setCart(cart);
+			cartItem.setQuantity(req.getQuantity());
+			cartItem.setUserId(userId);
+			
+			int price = req.getQuantity() * product.getDiscountedPrice();
+			cartItem.setPrice(price);
+			cartItem.setSize(req.getSize());
+			
+			CartItem createdCartItem = cartItemService.createCartItem(cartItem);
+			cart.getCartItems().add(createdCartItem);
+		}
+		
+		return "Item Add To Cart";
 	}
 
 	@Override
 	public Cart findUserCart(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Cart cart = cartRepository.findByUserId(userId);
+		
+		int totalPrice = 0;
+		int totalDiscountedPrice  = 0;
+		int totalItem = 0;
+		
+		for(CartItem cartItem :cart.getCartItems()) {
+			totalPrice =totalPrice + cartItem.getPrice();
+			totalDiscountedPrice = totalDiscountedPrice + cartItem.getDiscountedPrice();
+			totalItem = totalItem + cartItem.getQuantity();
+		}
+		
+		cart.setTotalPrice(totalPrice);
+		cart.setTotalDiscountedPrice(totalDiscountedPrice);
+		cart.setTotalItem(totalItem);
+		cart.setDiscounte(totalPrice - totalDiscountedPrice);
+		
+		
+		return cartRepository.save(cart);
 	}
 
 }
