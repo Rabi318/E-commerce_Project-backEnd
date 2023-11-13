bf432ca786122542d3b1b6251172bc6e41be7cf9
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-11-13 08:00:55 +0530

    CartController

---
+++ src/main/java/com/coder/controller/CartController.java
@@ -0,0 +1,56 @@
+package com.coder.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coder.exception.ProductException;
+import com.coder.exception.UserException;
+import com.coder.model.Cart;
+import com.coder.model.User;
+import com.coder.request.AddItemRequest;
+import com.coder.response.ApiResponse;
+import com.coder.service.CartService;
+import com.coder.service.UserService;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+	private CartService cartService;
+	private UserService userService;
+	
+	public CartController(CartService cartService, UserService userService) {
+		
+		this.cartService = cartService;
+		this.userService = userService;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<Cart> findUserCartHandler(@RequestHeader("Authorization") String jwt) throws UserException{
+		User user = userService.findUserProfileByJwt(jwt);
+		
+		Cart cart = cartService.findUserCart(user.getId());
+		System.out.println("Cart - "+cart.getUser().getEmail());
+		return new ResponseEntity<Cart>(cart,HttpStatus.OK);
+		
+	}
+	
+	@PutMapping("/add")
+	public ResponseEntity<ApiResponse> addItemToCart(@RequestBody AddItemRequest req, @RequestHeader("Authorization") String jwt) throws UserException, ProductException{
+		
+		User user = userService.findUserProfileByJwt(jwt);
+		
+		cartService.addCartItem(user.getId(), req);
+		
+		ApiResponse res = new ApiResponse("Item Added To Cart Seccessfuly", true);
+		
+		return new ResponseEntity<ApiResponse>(res,HttpStatus.ACCEPTED);
+	}
+}
